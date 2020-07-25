338f4180e35bc87a2c590c1e84bb61c49265b668
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2020-07-25 13:38:05 +0200

    Add sass support and change default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
-    <nuxt />
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="~assets/logo.png" alt="Logo"> Gotta Fetch 'Em All
+        </a>
+      </div>
+    </nav>
+
+    <section class="main-content">
+      <div class="container">
+        <nuxt />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
+
 }
 </script>
+
+<style lang="scss">
+  .navbar-item {
+    img {
+      margin-right: 5px;
+    }
+  }
+</style>
